9cdc03e44481c6e8a741c5762d237e3f2f057176
Công Đẹp Trai <150657350+CongLeDepTrai@users.noreply.github.com>
2023-11-29 17:36:21 +0700

    Add files via upload

---
+++ src/main/java/com/be/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.be.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.be.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, String> {
+	
+}
